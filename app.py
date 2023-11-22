d6272881b937249cc7c3e954d21bc539f9efb304
raflingr <130273233+raflingr@users.noreply.github.com>
2023-11-22 09:11:22 +0000

    update app

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from sqlalchemy import text
 
-list_doctor = ['', 'dr. Tirta', 'dr. Yogi', 'dr. Wibowo', 'dr. Ulama', 'dr. Ping']
+list_doctor = ['', 'dr. Nurita', 'dr. Yogi', 'dr. Wibowo', 'dr. Ulama', 'dr. Ping']
 list_symptom = ['', 'male', 'female']
 
 conn = st.connection("postgresql", type="sql",  
