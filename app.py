a97e81bd28642443d4606df8b9d422ad03acae51
raflingr <130273233+raflingr@users.noreply.github.com>
2023-11-22 10:19:02 +0000

    update app

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ with conn.session as session:
                                                        symptom text, handphone varchar, address text, tanggal date);')
     session.execute(query)
 
-st.header('SIMPLE HOSPITAL DATA MANAGEMENT SYSTEM')
+st.header('COY HOSPITAL DATA MANAGEMENT SYSTEM')
 page = st.sidebar.selectbox("Pilih Menu", ["View Data","Edit Data"])
 
 if page == "View Data":
