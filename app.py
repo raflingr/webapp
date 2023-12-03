2b69fd2569c7ab30fb496cabaf591d518780434c
raflingr <130273233+raflingr@users.noreply.github.com>
2023-12-03 10:37:50 +0000

    update app

--- app.py
+++ app.py
@@ -15,33 +15,36 @@ with engine.connect() as conn:
     conn.execute(query)
 
 st.header('HOTEL RESERVATIONS & CAFE DATA MANAGEMENT SYSTEM')
-
-list_doctor = ['', 'dr. Nurita', 'dr. Yogi', 'dr. Wibowo', 'dr. Ulama', 'dr. Ping']
-list_symptom = ['', 'male', 'female']
-
-conn = st.connection("postgresql", type="sql",  
-                     url="postgresql://raflinugrahasyach26:OVv3xh7JBDiY@ep-round-dust-26397985.us-east-2.aws.neon.tech/web")
-with conn.session as session:
-    query = text('CREATE TABLE IF NOT EXISTS SCHEDULE (id serial, doctor_name varchar, patient_name varchar, gender char(25), \
-                                                       symptom text, handphone varchar, address text, tanggal date);')
-    session.execute(query)
-
-st.header('HOTEL RESERVATIONS & CAFE DATA MANAGEMENT SYSTEM')
-page = st.sidebar.selectbox("Pilih Menu", ["View Data","Edit Data"])
+page = st.sidebar.selectbox("Pilih Menu", ["View Data", "Edit Data"])
 
 if page == "View Data":
-    data = conn.query('SELECT * FROM schedule ORDER By id;', ttl="0").set_index('id')
+    data = pd.read_sql_query('SELECT * FROM schedule ORDER By id;', engine).set_index('id')
     st.dataframe(data)
+
+    # Visualize data using seaborn
+    st.subheader("Data Visualization")
+    
+    # Countplot for Gender
+    fig, ax = plt.subplots()
+    sns.countplot(x='gender', data=data, ax=ax)
+    st.pyplot(fig)
+    
+    # Bar plot for Doctor-wise patient count
+    fig, ax = plt.subplots()
+    sns.countplot(x='doctor_name', data=data, ax=ax)
+    plt.xticks(rotation=45, ha='right')  # Rotate x-axis labels
+    st.pyplot(fig)
+
+    # Custom visualizations can be added based on your data and requirements
 
 if page == "Edit Data":
     if st.button('Tambah Data'):
-        with conn.session as session:
+        with engine.connect() as conn:
             query = text('INSERT INTO schedule (doctor_name, patient_name, gender, symptom, handphone, address, waktu, tanggal) \
                           VALUES (:1, :2, :3, :4, :5, :6, :7, :8);')
-            session.execute(query, {'1':'', '2':'', '3':'', '4':'[]', '5':'', '6':'', '7':None, '8':None})
-            session.commit()
+            conn.execute(query, {'1': '', '2': '', '3': '', '4': '[]', '5': '', '6': '', '7': None, '8': None})
 
-    data = conn.query('SELECT * FROM schedule ORDER By id;', ttl="0")
+    data = pd.read_sql_query('SELECT * FROM schedule ORDER By id;', engine)
     for _, result in data.iterrows():        
         id = result['id']
         doctor_name_lama = result["doctor_name"]
@@ -68,38 +71,16 @@ if page == "Edit Data":
 
                 with col1:
                     if st.form_submit_button('UPDATE'):
-                        with conn.session as session:
+                        with engine.connect() as conn:
                             query = text('UPDATE schedule \
                                           SET doctor_name=:1, patient_name=:2, gender=:3, symptom=:4, \
                                           handphone=:5, address=:6, waktu=:7, tanggal=:8 \
                                           WHERE id=:9;')
-                            session.execute(query, {'1':doctor_name_baru, '2':patient_name_baru, '3':gender_baru, '4':str(symptom_baru), 
-                                                    '5':handphone_baru, '6':address_baru, '7':waktu_baru, '8':tanggal_baru, '9':id})
-                            session.commit()
-                            st.experimental_rerun()
-                
+                            conn.execute(query, {'1': doctor_name_baru, '2': patient_name_baru, '3': gender_baru, '4': str(symptom_baru), 
+                                                 '5': handphone_baru, '6': address_baru, '7': waktu_baru, '8': tanggal_baru, '9': id})
+
                 with col2:
                     if st.form_submit_button('DELETE'):
-                        query = text(f'DELETE FROM schedule WHERE id=:1;')
-                        session.execute(query, {'1':id})
-                        session.commit()
-                        st.experimental_rerun()
-# ... (Your existing code)
-if page == "View Data":
-    data = pd.read_sql_query('SELECT * FROM schedule ORDER By id;', engine).set_index('id')
-    st.dataframe(data)
-
-    # Visualize data using seaborn
-    st.subheader("Data Visualization")
-    
-    # Countplot for Gender
-    fig, ax = plt.subplots()
-    sns.countplot(x='gender', data=data, ax=ax)
-    st.pyplot(fig)
-    
-    # Bar plot for Doctor-wise patient count
-    fig, ax = plt.subplots()
-    sns.countplot(x='doctor_name', data=data, ax=ax)
-    st.pyplot(fig)
-
-    # Custom visualizations can be added based on your data and requirements
+                        with engine.connect() as conn:
+                            query = text(f'DELETE FROM schedule WHERE id=:1;')
+                            conn.execute(query, {'1': id})
