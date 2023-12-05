635a0ea3c0d47c88ce543f4c17072885426d1b7b
raflingr <130273233+raflingr@users.noreply.github.com>
2023-12-05 06:45:06 +0000

    update app

--- app.py
+++ app.py
@@ -1,107 +1,169 @@
 import streamlit as st
-import pandas as pd
-from sqlalchemy import create_engine, text
-import seaborn as sns
-import matplotlib.pyplot as plt
+from sqlalchemy import text
 
-# Data reservasi
-data_reservasi = pd.DataFrame({
-    'Pertanyaan': ['Nama', 'Makan:', 'Minum:', 'Pembayaran:', 'No Kamar:', 'No Meja:'],
-    'Type': ['text', 'chose multiple', 'chose multiple', 'chose bersyarat', 'text', 'text'],
-    'Harga': [None, {'Nasi Goreng': 22000, 'Mie Goreng': 10000, 'Ayam Goreng': 15000},
-              {'Es Teh': 8000, 'Es Jeruk': 8000, 'Air Mineral': 5000}, None, None, None],
-    'Value': [None, None, None, None, None, None]
-})
+list_room = ['', 'twin deluxe', 'double bed', 'premium intermediate', 'business premium', 'diamond class']
+list_gender = ['', 'male', 'female']
+list_payment = ['', 'ATM', 'Transfer', 'Tunai']
+list_metode = ['','Diantar', 'Ditunggu']
 
-# Create an SQLAlchemy engine
-engine = create_engine("postgresql://raflinugrahasyach26:OVv3xh7JBDiY@ep-round-dust-26397985.us-east-2.aws.neon.tech/web")
+conn = st.connection("postgresql", type="sql", 
+                     url="postgresql", type="sql", 
+                     url="postgresql://raflinugrahasyach26:OVv3xh7JBDiY@ep-round-dust-26397985.us-east-2.aws.neon.tech/web")
+with conn.session as session:
+    query_room = text('CREATE TABLE IF NOT EXISTS hotel_room (id serial, nama text, gender varchar, contact text, series_room varchar, other_needs text, \
+                                                       check_in date, time_ci time, check_out date, time_co time, payment text, price text);')
+    query_restaurant = text('CREATE TABLE IF NOT EXISTS hotel_restaurant (id serial, pelanggan text, makanan varchar, jumlah_makanan integer, minuman varchar, \
+                                                       jumlah_minuman integer, metode text, no_tempat text, total_harga integer, pembayaran text);')
+    session.execute(query_room)
+    session.execute(query_restaurant)
 
-# Membuat tabel baru jika belum ada
-with engine.connect() as connection:
-    query = text('CREATE TABLE IF NOT EXISTS RESERVATION (id serial, \
-                                                       Nama varchar, \
-                                                       Makan text[], \
-                                                       Minum text[], \
-                                                       Pembayaran varchar, \
-                                                       No_Kamar varchar, \
-                                                       No_Meja varchar, \
-                                                       PRIMARY KEY (id));')
-    connection.execute(query)
+def home():
+    st.title('DIAMOND LUXURY TOWER HOTEL')
+    st.header('DATABASE INTERNAL STAFF')
 
-# Tampilkan form untuk mengisi data
-st.header('CAFE HOTEL NUANSA LIMA')
-page = st.sidebar.selectbox("Reservasi Cafe", ["View Data", "Edit Data"])
+    image_path = "https://www.theluxevoyager.com/wp-content/uploads/2018/02/Four-Seasons-Hotel-Jakarta-pool.jpg"
+    st.image(image_path, caption='Warning! Database hanya diakses oleh karyawan/staff hotel')
 
-if page == "View Data":
-    # Dummy data untuk ditampilkan
-    dummy_data = [
-        {'Nama': 'John Doe', 'Makan': ['Nasi Goreng', 'Ayam Goreng'], 'Minum': ['Es Teh', 'Air Mineral'],
-         'Pembayaran': 'Debit', 'No Kamar': '101', 'No Meja': 'A1', 'Total Harga': 27000},
-        {'Nama': 'Jane Doe', 'Makan': ['Mie Goreng', 'Ayam Goreng'], 'Minum': ['Es Jeruk', 'Air Mineral'],
-         'Pembayaran': 'Cash', 'No Kamar': '102', 'No Meja': 'A2', 'Total Harga': 28000},
-    ]
+    st.subheader("Pelayanan :")
+    st.markdown("- Penyimpanan Data Room Hotel")
+    st.markdown("- Penyimpanan Data Restaurant")
+    st.markdown("- Penambahan Data")
+    st.markdown("- Visualisasi Data")
 
-    # Tampilkan data dalam bentuk tabel
-    st.subheader('Data Reservasi')
-    st.table(dummy_data)
+def room_hotel():
+    st.header('Reservation Room Hotel Diamond Luxury Tower Hotel')
+    page = st.sidebar.selectbox("Room Hotel", ['View Data Room Hotel', 'Additing Data Room Hotel'])
 
-    # Data Visualization
-    data = pd.read_sql_query('SELECT * FROM RESERVATION ORDER By id;', con=engine).set_index('id')
-    st.dataframe(data)
+    if page == "View Data Room Hotel":
+        data = conn.query('Select * FROM hotel_room ORDER BY id;', ttl="0").set_index('id')
+        st.dataframe(data)
+    
+    if page == "Additing Data Room Hotel":
+        if st.button('Tambah Data'):
+            with conn.session as session:
+                query_room = text('INSERT INTO hotel_room ("nama", "gender", "contact", "series_room", "other_needs", "check_in", "time_ci", "check_out", "time_co", "payment", "price") \
+                        VALUES (:1, :2, :3, :4, :5, :6, :7, :8, :9, :10, :11);')
+                session.execute(query_room, {'1':'', '2':'', '3':'', '4':'', '5':None, '6':'', '7':'', '8':'', '9':'', '10':'', '11':['Rp']})
+                session.commit()
+        
+    data = conn.query('SELECT * FROM hotel_room ORDER By id;', ttl="0")
+    for _, result in data.iterrows(): 
+        id = result['id']
+        nama_lama = result.loc["nama"]
+        gender_lama = result["gender"]
+        contact_lama = result["contact"]
+        series_room_lama = result["series_room"]
+        other_needs_lama = result["other_needs"]
+        check_in_lama = result["check_in"]
+        time_ci_lama = result["time_ci"]
+        check_out_lama = result["check_out"]
+        time_co_lama = result["time_co"]
+        payment_lama = result["payment"]
+        price_lama = result["price"]
 
-    # Visualize data using seaborn
-    st.subheader("Data Visualization")
+    with st.expander(f'a.n. {nama_lama}'):
+        with st.form(f'data-{id}'):
+            nama_baru = st.text_input("nama", nama_lama)
+            gender_baru = st.selectbox("gender", list_gender, list_gender.index(gender_lama))
+            contact_baru = st.text_input("contact", contact_lama)
+            series_room_baru = st.selectbox("series_room", list_room, list_room.index(series_room_lama) if series_room_lama in list_room else 0) 
+            other_needs_baru = st.text_input("other_needs", other_needs_lama)
+            check_in_baru = st.date_input("check_in", check_in_lama)
+            time_ci_baru = st.time_input("time_ci", time_ci_lama)
+            check_out_baru = st.date_input("check_out", check_out_lama)
+            time_co_baru = st.time_input("time_co", time_co_lama)
+            payment_baru = st.selectbox("payment", list_payment, list_payment.index(payment_lama))
+            price_baru = st.text_input("price", price_lama)
+                
+            col1, col2 = st.columns([1, 6])
 
-    # Countplot for Makan
-    st.subheader("Makan")
-    makan_counts = [makan for sublist in data['Makan'].dropna() for makan in sublist]
-    makan_counts_series = pd.Series(makan_counts)
-    sns.countplot(x=makan_counts_series, order=makan_counts_series.value_counts().index)
-    st.pyplot()
+            with col1:
+                    if st.form_submit_button('UPDATE'):
+                        with conn.session as session:
+                            query_room = text('UPDATE hotel_room \
+                                          SET nama=:1, gender=:2, contact=:3, series_room=:4, other_needs=:5 \
+                                          check_in=:6, time_ci=:7, check_out=:8, time_co=:9, payment=:10, price=:11 \
+                                          WHERE id=:12;')
+                            session.execute(query_room, {'1':nama_baru, '2':gender_baru, '3':contact_baru, '4':series_room_baru, '5':other_needs_baru, 
+                                                    '6':check_in_baru, '7':time_ci_baru, '8':check_out_baru, '9':time_co_baru, '10':payment_baru, '11':price_baru,
+                                                    '12':id})
+                            session.commit()
+                            st.experimental_rerun()
 
-    # Countplot for Minum
-    st.subheader("Minum")
-    minum_counts = [minum for sublist in data['Minum'].dropna() for minum in sublist]
-    minum_counts_series = pd.Series(minum_counts)
-    sns.countplot(x=minum_counts_series, order=minum_counts_series.value_counts().index)
-    st.pyplot()
+            with col2:
+                    if st.form_submit_button('DELETE'):
+                        query_room = text(f'DELETE FROM hotel_room WHERE id=:1;')
+                        session.execute(query_room, {'1':id})
+                        session.commit()
+                        st.experimental_rerun()
 
-    # Bar plot for Pembayaran
-    st.subheader("Pembayaran")
-    sns.countplot(x='Pembayaran', data=data)
-    st.pyplot()
+def restaurant_hotel():
+    st.header('Reservation Restaurant Diamond Luxury Tower Hotel')
 
-if page == "Edit Data":
-    st.subheader('Form Reservasi')
-    total_harga = 0  # Inisialisasi total harga
+    page = st.sidebar.selectbox("Restaurant Hotel", ['View Data Restaurant', 'Additing Data Restaurant'])
 
-    for _, row in data_reservasi.iterrows():
-        pertanyaan = row['Pertanyaan']
-        tipe_data = row['Type']
+    if page == "View Data Restaurant":
+        data = conn.query('Select * FROM hotel_restaurant ORDER BY id;', ttl="0").set_index('id')
+        st.dataframe(data)
+    
+    if page == "Additing Data Restaurant":
+        if st.button('Tambah Data'):
+            with conn.session as session:
+                query_restaurant = text('INSERT INTO hotel_restaurant ("pelanggan", "makanan", "jumlah_makanan", "minuman", "jumlah_minuman", "metode", "no_tempat", "total_harga", "pembayaran") \
+                        VALUES (:1, :2, :3, :4, :5, :6, :7, :8, :9);')
+                session.execute(query_restaurant, {'1':'', '2':None, '3':0, '4':None, '5':0, '6':'', '7':'', '8':0, '9':''})
+                session.commit()
+        
+    data = conn.query('SELECT * FROM hotel_restaurant ORDER By id;', ttl="0")
+    for _, result in data.iterrows():
+        id = result['id']
+        pelanggan_lama = result.loc["pelanggan"]
+        makanan_lama = result["makanan"]
+        jumlah_makanan_lama = result["jumlah_makanan"]
+        minuman_lama = result["minuman"]
+        jumlah_minuman_lama = result["jumlah_minuman"]
+        metode_lama = result["metode"]
+        no_tempat_lama = result["no_tempat"]
+        total_harga_lama = result["total_harga"]
+        pembayaran_lama = result["pembayaran"]
 
-        if tipe_data == 'text':
-            jawaban = st.text_input(pertanyaan, key=f"{pertanyaan.strip(':')}_input")
-        elif tipe_data == 'integer':
-            jawaban = st.number_input(pertanyaan, key=f"{pertanyaan.strip(':')}_input")
-        elif tipe_data == 'chose multiple':
-            pilihan = row['Harga'].keys()  # Mengambil pilihan dari kolom Harga
-            jawaban = st.multiselect(pertanyaan, pilihan, key=f"{pertanyaan.strip(':')}_input")
-        elif tipe_data == 'chose bersyarat':
-            pilihan = ['Cash', 'Debit']  # Ganti dengan pilihan yang sesuai
-            jawaban = st.selectbox(pertanyaan, pilihan, key=f"{pertanyaan.strip(':')}_input")
+    with st.expander(f'a.n. {pelanggan_lama}'):
+        with st.form(f'data-{id}'):
+            pelanggan_baru = st.text_input("nama", pelanggan_lama)
+            makanan_baru = st.text_input("makanan", makanan_lama)
+            jumlah_makanan_baru = st.text_input("jumlah_makanan", jumlah_makanan_lama)
+            minuman_baru = st.text_input("minuman", minuman_lama) 
+            jumlah_minuman_baru = st.text_input("jumlah_minuman", jumlah_minuman_lama)
+            metode_baru = st.selectbox("metode", list_metode, list_metode.index(metode_lama))
+            no_tempat_baru = st.text_input("no_tempat", no_tempat_lama)
+            total_harga_baru = st.text_input("total_harga", total_harga_lama)
+            pembayaran_baru = st.selectbox("pembayaran", list_payment, list_payment.index(pembayaran_lama))
+                
+            col1, col2 = st.columns([1, 6])
 
-        # Update nilai di dalam data
-        data_reservasi.at[_, 'Value'] = jawaban
+            with col1:
+                    if st.form_submit_button('UPDATE'):
+                        with conn.session as session:
+                            query_restaurant = text('UPDATE hotel_restaurant \
+                                          SET pelanggan=:1, makanan=:2, jumlah_makanan=:3, minuman=:4, jumlah_minuman=:5 \
+                                          metode=:6, no_tempat=:7, total_harga=:8, pembayaran=:9, \
+                                          WHERE id=:10;')
+                            session.execute(query_restaurant, {'1':pelanggan_baru, '2':makanan_baru, '3':jumlah_makanan_baru, '4':minuman_baru, '5':jumlah_minuman_baru, 
+                                                    '6':metode_baru, '7':no_tempat_baru, '8':total_harga_baru, '9':pembayaran_baru, 
+                                                    '10':id})
+                            session.commit()
+                            st.experimental_rerun()
 
-        # Hitung total harga makanan dan minuman
-        if pertanyaan == 'Makan:':
-            total_harga += sum([row['Harga'][makanan] for makanan in jawaban])
-        elif pertanyaan == 'Minum:':
-            total_harga += sum([row['Harga'][minuman] for minuman in jawaban])
+            with col2:
+                    if st.form_submit_button('DELETE'):
+                        query_restaurant = text(f'DELETE FROM hotel_restaurant WHERE id=:1;')
+                        session.execute(query_restaurant, {'1':id})
+                        session.commit()
+                        st.experimental_rerun()
 
-    # Menampilkan total harga
-    st.write(f"Total Harga: {total_harga}")
-
-    # Tombol untuk menyimpan data reservasi
-    if st.button('Reservasi'):
-        st.success('Data reservasi berhasil disimpan!')
+if st.sidebar.checkbox("Room Hotel"):
+    room_hotel()
+if st.sidebar.checkbox("Restaurant Hotel"):
+    restaurant_hotel()
+else:
+    home()
--- app.sql
+++ app.sql
@@ -1,14 +1,41 @@
-drop table if exists RESERVATION;
-CREATE TABLE IF NOT EXISTS RESERVATION (id serial, \
-                                                       Nama varchar, \
-                                                       Makan text[], \
-                                                       Minum text[], \
-                                                       Pembayaran varchar, \
-                                                       No_Kamar varchar, \
-                                                       No_Meja varchar, \
-                                                       PRIMARY KEY (id));
+DROP TABLE IF EXISTS room;
 
-insert into RESERVATION (Nama, Makan, Minum, Pembayaran, No_Kamar, No_Meja) 
-values
-	{'John Doe', 'Nasi Goreng', 'Es Teh', 'Debit', '101', 'A1', '27000'},
-	{'John Doi', 'Mie Goreng', 'Es Teh', 'Debit', '103', 'A2', '28000'};
+CREATE TABLE room (
+    id serial,
+    nama text,
+    gender text,
+    contact text,
+    series_room text[],
+    other_needs text,
+    check_in date,
+    time_ci time,
+    check_out date,
+    time_co time,
+    payment text, 
+    price text 
+);
+
+INSERT INTO room (nama, gender, contact, series_room, other_needs, check_in, time_ci, check_out, time_co, payment, price) 
+VALUES
+    ('dr. Nurita', 'male', '0812121', ARRAY['twin deluxe'], 'water', '2023-10-01', '13:00', '2023-10-02', '08:00', 'Tunai', 280000),
+    ('Aldenia Boo', 'female', '0812122018', ARRAY['twin deluxe'], 'water & sandals', '2023-10-02', '14:00', '2023-10-04', '08:00', 'Transfer', 1050000);
+
+DROP TABLE IF EXISTS restaurant;
+
+CREATE TABLE restaurant (
+    id serial,
+    pelanggan text,
+    makanan text,
+    jumlah_makanan integer,
+    minuman text, 
+    jumlah_minuman integer,
+    metode text,
+    no_tempat text,
+    total_harga integer,
+    pembayaran text
+);
+
+INSERT INTO restaurant (pelanggan, makanan, jumlah_makanan, minuman, jumlah_minuman, metode, no_tempat, total_harga, pembayaran) 
+VALUES
+    ('dr. Nurita', 'Mie Ayam Spesial', '2', 'Air Putih', '4', 'Diantar', 'Kamar 203', '60.000', 'Tunai'),
+    ('Aldenia Boo', 'Nasi Goreng Udang, Mie Ayam Bakso', '1, 2', 'Es Jeruk'], '3', 'Ditunggu', 'Meja 8', '84.000', 'ATM');
